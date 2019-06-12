237938dc9122916b51b5b32625055bfad15a8a75
Alveona <pomavau@yandex.ru>
2019-06-12 15:16:57 +0300

    Implement admin image show

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django.conf import settings
+from .models import Image
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+     def image(self, obj):
+        print(obj)
+        return format_html('<img src="%s"/>' % (settings.BASE_URL + obj.img.url))
+     # admin_image.allow_tags = True
+     list_display = ('img', 'image')
+admin.site.register(Image, ImageAdmin)
